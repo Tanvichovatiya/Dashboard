6688e20b03971a62f1327107853c6ad8d05e3b25
tanvi chovatiya <chovatiyatanvi21@gmail.com>
2024-12-03 17:38:39 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import {store} from './feature/store'
 import Navbar from './component2/Navbar'
@@ -7,19 +7,16 @@ import TaskDashboard from './component2/TaskDashboard'
 import Sidebar from './component/sidebar'
 const App = () => {
   return (
-  <Provider store={store}>
-      <Router>
-        
-        <div className="container mx-auto p-4">
-          <Routes>
-          <Route path="/" element={<Sidebar/>} />
-
-            <Route path="/dashboard" element={<TaskDashboard />} />
-          </Routes>
-        </div>
-      </Router>
-      
-    </Provider>
+    <Provider store={store}>
+    <HashRouter>
+      <div className="container mx-auto p-4">
+        <Routes>
+          <Route path="/" element={<Sidebar />} />
+          <Route path="/dashboard" element={<TaskDashboard />} />
+        </Routes>
+      </div>
+    </HashRouter>
+  </Provider>
 
     
   );
